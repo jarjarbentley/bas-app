fadbeccbab25ab64067674022b692fd8e0c3d86f
Josh Bentley <josh.bentley@sap.com>
2022-11-15 14:56:54 -0500

    Update example.e2e.js

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -1,5 +1,5 @@
 describe('Test Sample App', () => {
     it('should return valid title on the page', async () => {
-        await expect($('#container-HTML5Module---View1--page-title')).toHaveTextContaining('Tile');
+        await expect($('#container-HTML5Module---View1--page-title')).toHaveTextContaining('Title');
     });
 });
